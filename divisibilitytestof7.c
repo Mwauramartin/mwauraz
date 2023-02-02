8901ae101d8b5012effac0eb149199020e0d2033
Mwauramartin <122536919+Mwauramartin@users.noreply.github.com>
2023-02-02 15:54:51 +0300

    Add files via upload

---
+++ divisibilitytestof7.c
@@ -0,0 +1,13 @@
+//divisibility test of 7
+#include<stdio.h>
+int main(){
+    int a;
+    printf("\n enter a value:");
+    scanf("%d",&a);
+    if(a%7==0){
+        printf("a is divisible by 7");}
+        else
+        printf("A is not divisible by 7");
+        
+        return 0;
+    }
