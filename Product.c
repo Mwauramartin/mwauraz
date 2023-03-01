fe2db83a1c9f94859ce11b7cb0ae45d9ddc7e7ff
Mwauramartin <122536919+Mwauramartin@users.noreply.github.com>
2023-03-01 15:10:34 +0300

    Create Product.c

---
+++ Product.c
@@ -0,0 +1,17 @@
+//Product
+#include<stdio.h>
+int product(int,int);
+int product(int, int);
+int main()
+{
+    int product;
+    int a,b;
+    printf("\n enter value of a:");
+    printf("\n enter value of b:");
+    scanf("%d%d",&a,&b);
+    
+    return 0;
+}
+int product(int a,int b){
+    return a*b;
+}
