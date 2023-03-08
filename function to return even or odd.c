0be92823bda1181e19fc6c2622cea736651cc596
Mwauramartin <122536919+Mwauramartin@users.noreply.github.com>
2023-03-08 10:00:39 +0300

    Add files via upload

---
+++ function to return even or odd.c
@@ -0,0 +1,18 @@
+// function to return if a number is even or odd
+#include<stdio.h>
+int k;
+int main()
+{
+  int a;
+  printf("\n enter value of k:");
+  scanf("%d",&k);
+}
+//function definition
+int evenorodd(int k)
+{
+    if(k%2==0)
+    printf("%d is even:",k);
+    else
+    printf("%d is odd:",k);
+    return 0;
+}
